2025891ce330235e8b0acc1b515b4c51ce8c1e0a
mangeshp.92 <mangeshp.92@gmail.com>
2017-11-01 18:00:40 +0530

    check test

---
+++ config/test.php
@@ -0,0 +1,13 @@
+<?php
+return [
+
+    'dsnVar' => 'DRIVER={Oracle11.2};(DESCRIPTION =
+     (ADDRESS = (PROTOCOL = TCP)(HOST = 103.230.37.62)(PORT = 1521))
+     (CONNECT_DATA =
+       (SERVER = DEDICATED)
+       (SERVICE_NAME = VASTUPROD)
+     )
+   )',
+
+   'dsnTest'=>'test2',
+];
